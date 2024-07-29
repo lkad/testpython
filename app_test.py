6c9b4f5bc1768e20522078ee006527d30c3e3be8
codebase <codebase@edp.local>
2024-07-29 03:12:42 +0000

    Initial commit

---
+++ app_test.py
@@ -0,0 +1,10 @@
+"""Unit test for application."""
+from app import app
+
+
+def test_hello():
+    """Returns Hello, World."""
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
